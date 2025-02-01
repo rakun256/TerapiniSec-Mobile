c2cb48e1fdf719cbb67a06c3855504e48060fd1a
Emre Uslu <75951256+rakun256@users.noreply.github.com>
2025-02-01 17:04:13 +0000

    Feb 1, 2025, 8:04 PM

---
+++ app/utils/data/userData.js
@@ -0,0 +1,36 @@
+// app/utils/data/usersData.js
+export const usersData = [
+    {
+      id: 1,
+      userName: "emreuslu",
+      firstName: "Emre",
+      lastName: "Uslu",
+      birthday: "2004-10-01",
+      phoneNumber: "+905380412101",
+      email: "emreuslu256@gmail.com",
+      password: "emresifre", // Gerçek bir projede bu şifreleri şifrelenmiş olarak saklamalısın.
+      userRole: "ADMIN",
+      registrationDateTime: "2025-01-01T10:00:00Z",
+      lastLoginDateTime: "2025-01-30T18:30:00Z",
+      isPremium: true,
+      premiumStartDateTime: "2025-01-01T10:00:00Z",
+      premiumEndDateTime: null,
+      specializations: [],
+      yearsOfExperience: null,
+      availableTimes: []
+    },
+    {
+      id: 2,
+      userName: "ecem_nur",
+      firstName: "Ecem",
+      lastName: "Özen",
+      birthday: "2001-03-15",
+      phoneNumber: "+905555555556",
+      email: "ecem@example.com",
+      password: "hashedpassword2",
+      userRole: "PSYCHOLOGIST",
+      registrationDateTime: "2025-01-02T12:00:00Z",
+      lastLoginDateTime: "2025-01-30T19:00:00Z",
+      isPremium: true,
+      premiumStartDateTime: "202
+  
